d064958d0cfb94cbbff4228f154d708843fb8955
jeromeski <jeromegee@gmail.com>
2021-05-18 10:14:11 +0800

    ENV and Toast Notifications

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,20 @@ import Home from './pages/Home'
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Header from './components/nav/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
 	return (
 		<React.Fragment>
-        <Header />
-				<Switch>
-          <Route exact path='/' component={Home} />
-					<Route exact path='/login' component={Login} />
-					<Route exact path='/register' component={Register} />
-				</Switch>
+			<Header />
+			<ToastContainer autoClose={5000} hideProgressBar={false} />
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/register' component={Register} />
+			</Switch>
 		</React.Fragment>
 	);
 };
