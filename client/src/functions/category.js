c1272abc70d7b098787f9213dc9f687bc4fc165c
jeromeski <jeromegee@gmail.com>
2021-05-28 17:56:45 +0800

    FrontEnd - CRUD category API fns

---
+++ client/src/functions/category.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export const getCategories = async () => {
+	return await axios.get(`${process.env.REACT_APP_API}/categories`);
+};
+
+export const getCategory = async (slug) => {
+	return await axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
+};
+
+export const createCategory = async (slug, category, authtoken) => {
+	return await axios.post(`${process.env.REACT_APP_API}/category/${slug}`, category, {
+		headers: {
+			authtoken
+		}
+	});
+};
+
+export const updateCategory = async (slug, category, authtoken) => {
+	return await axios.put(`${process.env.REACT_APP_API}/category/${slug}`, category, {
+		headers: {
+			authtoken
+		}
+	});
+};
+
+export const deleteCategory = async (slug, category, authtoken) => {
+	return await axios.delete(`${process.env.REACT_APP_API}/category/${slug}`, category, {
+		headers: {
+			authtoken
+		}
+	});
+};
