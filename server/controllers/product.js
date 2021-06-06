b0241b0666ed248ddabd978a6f9dedba44afd2b4
jeromeski <jeromegee@gmail.com>
2021-06-06 17:32:40 +0800

    Product delete endpoint

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -25,4 +25,12 @@ exports.listAll = async (req, res) => {
 	res.json(products);
 };
 
-
+exports.remove = async (req, res) => {
+	try {
+		let deleted = await Product.findOneAndRemove({ slug: req.params.slug }).exec();
+		res.json(deleted);
+	} catch (err) {
+		console.log(err);
+		return res.status(400).send("Product delete failed");
+	}
+};
