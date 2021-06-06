4bb51a4c610d71e66788eb73a789e0d1ea535f88
jeromeski <jeromegee@gmail.com>
2021-06-06 14:16:26 +0800

    Updated product model added HP and Toshiba brands

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -18,9 +18,9 @@ exports.create = async (req, res) => {
 exports.listAll = async (req, res) => {
 	let products = await Product.find({})
 		.limit(parseInt(req.params.count))
-		.populate('category')
-		.populate('subs')
-		.sort(['createdAt', 'desc'])
+		.populate("category")
+		.populate("subs")
+		.sort([["createdAt", "desc"]])
 		.exec();
 	res.json(products);
 };
--- server/models/product.js
+++ server/models/product.js
@@ -30,12 +30,12 @@ const productSchema = new mongoose.Schema(
 		},
 		category: {
 			type: ObjectId,
-			ref: 'Category'
+			ref: "Category"
 		},
 		subs: [
 			{
 				type: ObjectId,
-				ref: 'Sub'
+				ref: "Sub"
 			}
 		],
 		quantity: Number,
@@ -48,15 +48,15 @@ const productSchema = new mongoose.Schema(
 		// },
 		shipping: {
 			type: String,
-			enum: ['Yes', 'No']
+			enum: ["Yes", "No"]
 		},
 		color: {
 			type: String,
-			enum: ['Black', 'Brown', 'Silver', 'White', 'Blue']
+			enum: ["Black", "Brown", "Silver", "White", "Blue"]
 		},
 		brand: {
 			type: String,
-			enum: ['Apple', 'Samsung', 'Microsoft', 'Lenovo', 'ASUS']
+			enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS", "HP", "Toshiba"]
 		}
 		// ratings: [
 		//   {
