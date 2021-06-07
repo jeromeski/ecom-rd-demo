9b20bdd65074f692d3c91f6c35cf494f78e48027
jeromeski <jeromegee@gmail.com>
2021-06-07 09:52:47 +0800

    ProductUpdate Component Page

--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
-import { useSelector } from "react-redux";
+
 import { getProduct } from "../../../functions/product";
 import { getCategories, getCategorySubs } from "../../../functions/category";
-import FileUpload from "../../../components/forms/FileUpload";
-import { LoadingOutlined } from "@ant-design/icons";
+import ProductUpdateForm from "../../../components/forms/ProductUpdateForm";
 
 const initialState = {
   title: "",
@@ -27,7 +26,6 @@ const ProductUpdate = ({ match }) => {
   // state
   const [values, setValues] = useState(initialState);
 
-  const { user } = useSelector((state) => ({ ...state }));
   // router
   const { slug } = match.params;
 
@@ -42,21 +40,36 @@ const ProductUpdate = ({ match }) => {
     });
   };
 
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-2">
-          <AdminNav />
-        </div>
+  const handleSubmit = (e) => {
+		e.preventDefault();
+	};
 
-        <div className="col-md-10">
-          <h4>Product update</h4>
-          {JSON.stringify(values)}
-          <hr />
-        </div>
-      </div>
-    </div>
-  );
+	const handleChange = (e) => {
+		//
+		setValues({ ...values, [e.target.name]: e.target.value });
+	};
+
+  return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-2">
+					<AdminNav />
+				</div>
+
+				<div className="col-md-10">
+					<h4>Product update</h4>
+					{/*JSON.stringify(values)*/}
+					<ProductUpdateForm
+						handleSubmit={handleSubmit}
+						handleChange={handleChange}
+						values={values}
+            setValues={setValues}
+					/>
+					<hr />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProductUpdate;
