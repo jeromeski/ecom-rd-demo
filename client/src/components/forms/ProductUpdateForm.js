c999da34c491e6b925008a1eb8e2b0035f68eb93
jeromeski <jeromegee@gmail.com>
2021-06-07 09:58:33 +0800

    Prepopulate shipping and brand

--- client/src/components/forms/ProductUpdateForm.js
+++ client/src/components/forms/ProductUpdateForm.js
@@ -64,8 +64,7 @@ const ProductUpdateForm = ({
 
 			<div className="form-group">
 				<label>Shipping</label>
-				<select name="shipping" className="form-control" onChange={handleChange}>
-					<option>Please select</option>
+				<select value={shipping === 'Yes' ? 'Yes': 'No' } name="shipping" className="form-control" onChange={handleChange}>
 					<option value="No">No</option>
 					<option value="Yes">Yes</option>
 				</select>
@@ -84,8 +83,7 @@ const ProductUpdateForm = ({
 
 			<div className="form-group">
 				<label>Color</label>
-				<select name="color" className="form-control" onChange={handleChange}>
-					<option>Please select</option>
+				<select value={color} name="color" className="form-control" onChange={handleChange}>
 					{colors.map((c) => (
 						<option key={c} value={c}>
 							{c}
@@ -96,8 +94,7 @@ const ProductUpdateForm = ({
 
 			<div className="form-group">
 				<label>Brand</label>
-				<select name="brand" className="form-control" onChange={handleChange}>
-					<option>Please select</option>
+				<select value={brand} name="brand" className="form-control" onChange={handleChange}>
 					{brands.map((b) => (
 						<option key={b} value={b}>
 							{b}
