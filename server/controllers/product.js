b732d28387bbf26d67e515826c485365a22d5640
jeromeski <jeromegee@gmail.com>
2021-06-08 11:30:40 +0800

    List products with pagination

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -60,21 +60,44 @@ exports.update = async (req, res) => {
   }
 }
 
-exports.list = async (req,res) => {
-  try{
-    const {sort, order, limit} = req.body;
+// WITHOUT PAGINATION
+// exports.list = async (req,res) => {
+//   try{
+//     const {sort, order, limit} = req.body;
+//     // createdAt/updatedAt, desc/asc, 3
+//     const products = await Product.find({})
+//       .populate("category")
+//       .populate("subs")
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec()
+//     res.json(products)
+//   }catch(err){
+//     console.log(err)
+//   }
+// }
+
+// WITH PAGINATION
+exports.list = async (req, res) => {
+  try {
     // createdAt/updatedAt, desc/asc, 3
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 3; // 3
+
     const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
       .populate("category")
       .populate("subs")
       .sort([[sort, order]])
-      .limit(limit)
-      .exec()
-    res.json(products)
-  }catch(err){
-    console.log(err)
+      .limit(perPage)
+      .exec();
+
+    res.json(products);
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
 exports.productsCount = async (req, res) => {
 	let total = await Product.find({}).estimatedDocumentCount().exec();
