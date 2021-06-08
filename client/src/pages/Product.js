cbaf25f4928821479237a3da7b6ce352e6302cc8
jeromeski <jeromegee@gmail.com>
2021-06-08 14:19:09 +0800

    Single Product Component

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getProduct } from "../functions/product";
+import SingleProduct from "../components/cards/SingleProduct";
 
 const Product = ({ match }) => {
 	const [product, setProduct] = useState({});
@@ -19,7 +20,19 @@ const Product = ({ match }) => {
 			.catch((err) => console.log(err));
 	};
 
-	return <>{JSON.stringify(product)}</>;
+	return (
+		<>
+			<div className="container-fluid">
+				<div className="row pt-4">
+					<SingleProduct product={product} />
+				</div>
+
+				<div className="row">
+					<div>Related products</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Product;
