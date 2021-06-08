cde363c22038ffda35506d736703bee67d51dc26
jeromeski <jeromegee@gmail.com>
2021-06-08 13:32:12 +0800

    Pagination NewArrivals BestSeller

--- client/src/components/home/NewArrivals.js
+++ client/src/components/home/NewArrivals.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { getProducts } from "../../functions/product";
+import { getProducts, getProductsCount } from "../../functions/product";
 import ProductCard from "../cards/ProductCard";
 import LoadingCard from "../cards/LoadingCard";
+import { Pagination } from "antd";
 
 const NewArrivals = () => {
 	const [products, setProducts] = useState([]);
 	const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+	const [productsCount, setProductsCount] = useState(0);
 
 	useEffect(() => {
 		loadAllProducts();
+	}, [page]);
+
+	useEffect(() => {
+		getProductsCount().then((res) => {
+			setProductsCount(res.data);
+		});
 	}, []);
 
 	const loadAllProducts = () => {
 		setLoading(true);
-		getProducts("createdAt", "desc", 3)
+		getProducts("createdAt", "desc", page)
 			.then((res) => {
 				setLoading(false);
 				setProducts(res.data);
@@ -35,6 +44,15 @@ const NewArrivals = () => {
 						))}
 					</div>
 				)}
+				<div className="row">
+					<nav className="col-md-4 offset-md-4 text-center pt-5 p-3">
+						<Pagination
+							current={page}
+							total={(productsCount / 3) * 10}
+							onChange={(value) => setPage(value)}
+						/>
+					</nav>
+				</div>
 			</div>
 		</>
 	);
