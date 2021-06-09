03992a4240c7a98871101c2626830d3f57d4ddcb
jeromeski <jeromegee@gmail.com>
2021-06-09 08:01:19 +0800

    Rating for Modal

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -7,6 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Laptop from "../../images/laptop.png";
 import ProductListItems from "./ProductListItems";
 import StarRating from "react-star-ratings";
+import RatingModal from "../modal/RatingModal";
 
 
 const { TabPane } = Tabs;
@@ -37,14 +38,6 @@ const SingleProduct = ({ product }) => {
 
 			<div className="col-md-5">
 				<h1 className="bg-info p-3">{title}</h1>
-				<StarRating
-					name={_id}
-					numberOfStars={5}
-					rating={2}
-					changeRating={(newRating, name) => console.log("newRating", newRating, "name", name)}
-					isSelectable={true}
-					starRatedColor="red"
-				/>
 				<Card
 					actions={[
 						<>
@@ -53,7 +46,19 @@ const SingleProduct = ({ product }) => {
 						</>,
 						<Link to="/">
 							<HeartOutlined className="text-info" /> <br /> Add to Wishlist
-						</Link>
+						</Link>,
+						<RatingModal>
+							<StarRating
+								name={_id}
+								numberOfStars={5}
+								rating={2}
+								changeRating={(newRating, name) =>
+									console.log("newRating", newRating, "name", name)
+								}
+								isSelectable={true}
+								starRatedColor="red"
+							/>
+						</RatingModal>
 					]}>
 					<ProductListItems product={product} />
 				</Card>
