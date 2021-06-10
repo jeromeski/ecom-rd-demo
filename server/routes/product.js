b6c168cda1e83c7cf284c69c5c4585a2d2436ab3
jeromeski <jeromegee@gmail.com>
2021-06-10 09:08:34 +0800

    Updated product controllers to sync tut

--- server/routes/product.js
+++ server/routes/product.js
@@ -19,12 +19,14 @@ const {
 // routes
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/total", productsCount);
-router.get('/products/:count', listAll);
+
+router.get("/products/:count", listAll); // products/100
 router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
-router.post('/products', list);
-router.post("/product/star/:productId", authCheck, productStar);
 
+router.post("/products", list);
+// rating
+router.put("/product/star/:productId", authCheck, productStar);
 
 module.exports = router;
